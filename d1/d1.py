066fa4689e7d071d36d6474b9de58ef2fb956f2e
unknown <nesha002@umn.edu>
2021-12-03 14:34:39 -0700

    improve readability

--- d1/d1.py
+++ d1/d1.py
@@ -1,22 +1,14 @@
-# read in input file
 with open("d1Input.txt") as f:
     readings = f.readlines()
 
-# create vars to hold increase and decrease counts
-iCount = 0
-dCount = 0
-
 # increase counts based on readings grouped into 3s
+increase_count = 0
 for i,v in enumerate(readings):
     a = int(readings[i-1]) + int(readings[i-2]) + int(readings[i-3])
     b = int(readings[i]) + int(readings[i-1]) + int(readings[i-2])
-    # ignore the first readings as that will be out of range when calculating a and b
-    if i==0 or i==1 or i==2:
+    if i < 3:  # ignore the first readings as that will be out of range when calculating a and b
         continue
     elif b > a:
-        iCount+=1
-    else:
-        dCount+=1
+        increase_count+=1
 
-print("increases: ", iCount)
-print("decreases: ", dCount)
+print("increases: ", increase_count)
--- d2/d2.py
+++ d2/d2.py
@@ -1,50 +1,38 @@
-# calculate the overall position
 def calc_position(horizontal, depth):
     position = horizontal * depth
     return position
 
+def calc_aim(direction, new_value, current_aim):
+    if direction == "up":
+        new_aim = current_aim - new_value
+        return new_aim
+    else: # direction = down
+        new_aim = current_aim + new_value
+        return new_aim
 
-# calculate aim (aim added in challenge 2)
-def calc_aim(direction, x, current_aim):
-    try:
-        if direction == "up":
-            new_aim = current_aim - x
-            return (0, new_aim)
-        else: # direction = down
-            new_aim = current_aim + x
-            return (0, new_aim)
-    except:
-        print("Recieved an unexpected value.")
-
-
-# calculate horizontal 
-# challenge 2 has horizontal change affecting depth
-def calc_horizontal(x, current_horizontal, current_aim, current_depth):
-    new_horizontal = current_horizontal + x
-    new_depth = current_depth + (x * current_aim)
+def calc_horizontal_and_depth(new_value, current_horizontal, current_aim, current_depth):
+    new_horizontal = current_horizontal + new_value
+    new_depth = current_depth + (new_value * current_aim)
     return (new_horizontal, new_depth)
 
 
 def main():
-    # read input values
     with open("d2Input.txt") as f:
         directions = f.readlines()
 
-    # create vars to be calculated
     horizontal = 0
     depth = 0
     aim = 0
 
-    # calculations
-    for i,val in enumerate(directions):
+    for val in directions:
         val=val.split(" ")
         if val[0]=="forward":
-            new_h = calc_horizontal(int(val[1]), horizontal, aim, depth)
+            new_h = calc_horizontal_and_depth(int(val[1]), horizontal, aim, depth)
             horizontal = new_h[0]
             depth = new_h[1]
         else:
             new_a = calc_aim(val[0], int(val[1]), aim)
-            aim = new_a[1]
+            aim = new_a
     
     position = calc_position(horizontal, depth)
     print(position)
